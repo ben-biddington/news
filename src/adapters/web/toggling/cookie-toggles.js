bdd440b5379ceada0b06e799863a47a0211ec496
Ben Biddington <ben.biddington@gmail.com>
2020-07-03 09:05:01 +1200

    Formatting

--- src/adapters/web/toggling/cookie-toggles.js
+++ src/adapters/web/toggling/cookie-toggles.js
@@ -1,17 +1,17 @@
 class CookieToggles {
   constructor(cookie) {
-      // "__test=1; _ga=GA1.1.1954298143.1586659473; ajs_user_id=null; ajs_group_id=null;
-      const values = cookie.split(';').map(pair => pair.split('='));
+    // "__test=1; _ga=GA1.1.1954298143.1586659473; ajs_user_id=null; ajs_group_id=null;
+    const values = cookie.split(';').map(pair => pair.split('='));
       
-      this._parameters = {};
+    this._parameters = {};
 
-      values.forEach(value => {
-        this._parameters[value[0]] = value[1] || true;
-      });
+    values.forEach(value => {
+      this._parameters[value[0]] = value[1] || true;
+    });
   }
 
   get(name) {
-      return [ true, 'true', '1', ''].includes(this._parameters[name]);
+    return [ true, 'true', '1', ''].includes(this._parameters[name]);
   }
 }
 

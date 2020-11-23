35d0b1d0833d44fcac5d25636e18e186da8f9715
Ben Biddington <ben.biddington@gmail.com>
2020-11-23 20:32:30 +1300

    [VUE] Shorten delay

--- src/adapters/web/gui/flavours/vue/NewsPanel.vue
+++ src/adapters/web/gui/flavours/vue/NewsPanel.vue
@@ -34,7 +34,7 @@
 
 <style scoped>
 .fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
+  transition: opacity .1s;
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;

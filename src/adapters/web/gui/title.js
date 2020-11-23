322000e8af30b2d50e769f46d2fd8c6917104441
Ben Biddington <ben.biddington@gmail.com>
2020-11-23 20:33:05 +1300

    Remove logging

--- src/adapters/web/gui/title.js
+++ src/adapters/web/gui/title.js
@@ -1,10 +1,8 @@
 class Title {
     constructor(application, document) {
         this.title = document.title;
-        
+
         application.on(['lobsters-items-loaded', 'hacker-news-items-loaded'], e => {
-            console.log(JSON.stringify(e, null, 2));
-            
             document.title = e.items.length > 0 ? `* ${this.title}` : this.title;
         });
     }

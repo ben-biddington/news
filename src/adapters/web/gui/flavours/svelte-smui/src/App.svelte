16c851b794392a835644607b1b85a1509c8a54fb
Ben Biddington <ben.biddington@gmail.com>
2020-11-30 08:18:50 +1300

    ?

--- src/adapters/web/gui/flavours/svelte-smui/src/App.svelte
+++ src/adapters/web/gui/flavours/svelte-smui/src/App.svelte
@@ -1,25 +1,54 @@
-<div class="container">
-  <Button on:click={() => clicked++}>
-    <Icon class="material-icons">thumb_up</Icon>
-    <Label>Click Me</Label>
-  </Button>
-  <p class="mdc-typography--body1">
-    {#if clicked}
-      You've clicked the button {clicked} time{clicked === 1 ? '' : 's'}.
-    {:else}
-      <span class="grayed">You haven't clicked the button.</span>
-    {/if}
-  </p>
-</div>
-
 <script>
   import Button, {Label, Icon} from '@smui/button';
-
-  let clicked = 0;
+  import TopAppBar, {Row, Section, Title} from '@smui/top-app-bar';
+  import IconButton from '@smui/icon-button';
+  import Checkbox from '@smui/checkbox';
+  import FormField from '@smui/form-field';
+  let prominent = false;
+  let dense = false;
+  let secondaryColor = false;
 </script>
 
 <style>
-  .grayed {
-    opacity: .6;
+  .top-app-bar-container {
+    max-width: 480px;
+    min-width: 480px;
+    height: 320px;
+    border: 1px solid rgba(0,0,0,.1);
+    margin: 0 18px 18px 0;
+  }
+
+  .top-app-bar-container {
+    overflow: auto;
+    display: inline-block;
+  }
+
+  .flexy {
+    display: flex;
+    flex-wrap: wrap;
   }
 </style>
+
+<section>
+<Button variant="unelevated"><Label>Unelevated</Label></Button>
+  <div class="flexy">
+    <div class="top-app-bar-container">
+      <TopAppBar variant="static" {prominent} {dense} color={secondaryColor ? 'secondary' : 'primary'}>
+        <Row>
+          <Section>
+            <IconButton class="material-icons">menu</IconButton>
+            <Title>Static</Title>
+          </Section>
+          <Section align="end" toolbar>
+            <IconButton class="material-icons" aria-label="Download">file_download</IconButton>
+            <IconButton class="material-icons" aria-label="Print this page">print</IconButton>
+            <IconButton class="material-icons" aria-label="Bookmark this page">bookmark</IconButton>
+          </Section>
+        </Row>
+      </TopAppBar>
+      <div>
+        abc def
+      </div>
+    </div>
+  </div>
+</section>

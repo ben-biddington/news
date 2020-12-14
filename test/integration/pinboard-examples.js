687c77e08bb066439b5a16a0799810c3968b8fab
Ben Biddington <ben.biddington@gmail.com>
2020-12-14 15:48:34 +1300

    [pinboard] Add test list

--- test/integration/pinboard-examples.js
+++ test/integration/pinboard-examples.js
@@ -3,11 +3,16 @@ const expect = require('chai').expect;
 const { get }  = require('../../src/adapters/internet');
 const { list } = require('../../src/adapters/pinboard');
 
-describe('[pinboard]', async () => {
-    it('can list bookmarks', async () => {
+describe('[pinboard] can list bookmarks', async () => {
+    it('for example', async () => {
         // [i] Find token here
         const token = process.env.PINBOARD_TOKEN;
         
         const all = await list({ get }, token);
     });
+});
+
+describe('[pinboard] can add bookmarks', async () => {
+    it('sends the url');
+    it('sends the web page title (first need to *find* the title from the web page)');
 });

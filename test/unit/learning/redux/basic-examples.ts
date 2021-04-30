c0afb5eb76f890226d41090527f545aa7b612943
Ben Biddington <ben.biddington@gmail.com>
2021-04-30 18:14:35 +1200

    [learning] Some redux examples

---
+++ test/unit/learning/redux/basic-examples.ts
@@ -0,0 +1,48 @@
+import { expect } from '../../application-unit-test';
+
+import { createStore, Store as ReduxStore } from 'redux'
+
+interface State {
+  items: string [];
+}
+
+class Store {
+  private reduxStore: ReduxStore;
+
+  constructor() {
+    this.reduxStore = createStore<State, any, any, any>(this.items, { items: [] });
+  }
+
+  add(item: string) {
+    this.reduxStore.dispatch({ type: 'ADD', text: item});
+  }
+
+  subscribe(listener: (state: State) => void) {
+    this.reduxStore.subscribe(() => listener(this.reduxStore.getState()))
+  }
+
+  private items(state: State, action) {
+    switch (action.type) {
+      case 'ADD':
+        return { items: [ ...state.items, action.text ] }
+      default:
+        return state
+    }
+  }
+}
+
+describe('Subscribing to changes', async () => {
+  it("for example", async () => {
+    const store = new Store();
+
+    let result = [];
+
+    store.subscribe(state => result = state.items);
+
+    store.add('A');
+    store.add('B');
+    store.add('C');
+
+    expect(result).to.eql([ 'A', 'B', 'C' ]);
+  });
+});

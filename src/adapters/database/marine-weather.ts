c4d44b161c2d2311560be078c98b50d9f31d1bd6
Ben Biddington <ben.biddington@gmail.com>
2021-07-12 17:19:48 +1200

    Starting on storing screenshots

--- src/adapters/database/marine-weather.ts
+++ src/adapters/database/marine-weather.ts
@@ -1,11 +1,17 @@
 import { Database } from './internal/database';
 import { DevNullLog, Log } from '../../core/logging/log';
+import { format, set, parse, getUnixTime } from 'date-fns'
 
 export type Screenshot = {
   timestamp: Date;
   file: Buffer;
 }
 
+export type FilterOptions = {
+  dateMatching: Date;
+}
+
+// [i] https://www.sqlitetutorial.net/sqlite-date/
 export class MarineWeather {
   private database: Database;
   private log: Log;
@@ -19,9 +25,12 @@ export class MarineWeather {
     await this.database.open();
     await this.database.ex(
       'run',
-      `CREATE TABLE IF NOT EXISTS [screenshots] (
-        timestamp date
-      )`);
+      `
+        CREATE TABLE IF NOT EXISTS [screenshots] (
+          datestamp text,
+          timestamp text
+        )
+      `);
     
     await migrate(this.database);
   }
@@ -29,17 +38,21 @@ export class MarineWeather {
   async addScreenshot(screenshot: Screenshot) : Promise<void> {
     return this.database.ex(
       'run',
-      `INSERT INTO [screenshots] (timestamp, file) VALUES (@timestamp, @file)`, 
+      `INSERT INTO [screenshots] (datestamp, timestamp, file) VALUES (@datestamp, @timestamp, @file)`, 
       {
-          '@timestamp': screenshot.timestamp,
-          '@file':  screenshot.file,
+          '@datestamp': this.dateString(screenshot.timestamp),
+          '@timestamp': this.dateTimeString(screenshot.timestamp),
+          '@file':      screenshot.file,
       });
   }
 
-  async listScreenshots(): Promise<Screenshot[]> {
+  async listScreenshots(filterOptions: FilterOptions = null): Promise<Screenshot[]> {
+    if (filterOptions)
+      return this.find(filterOptions);
+
     return this.database.ex(
       'all',
-      `SELECT timestamp,file from [screenshots] ORDER BY timestamp`).
+      `SELECT timestamp,file from [screenshots] ORDER BY datestamp`).
       then(rows => {
         return rows.map(row => {
           return {
@@ -48,6 +61,62 @@ export class MarineWeather {
           }
         })
       });
+  }
+
+  // [i] https://stackoverflow.com/questions/49344517/sqlite-compare-dates-without-timestamp
+  private async find(filterOptions: FilterOptions = null): Promise<Screenshot[]> {
+    const midnight = set(filterOptions.dateMatching, { hours: 0, minutes: 0 });
+
+    console.log(`Filtering to screenshots matching between <${midnight}> and <${filterOptions.dateMatching}>`);
+
+      // await this.database.ex(
+      //   'all',
+      //   `
+      //     SELECT 
+      //       strftime('%s', timestamp) as ts, timestamp
+      //     FROM 
+      //       [screenshots] 
+      //     ORDER BY timestamp`).
+      //   then(rows => {
+      //     return rows.map(row => {
+      //       console.log('ts', row.ts, 'timestamp', row.timestamp);
+      //     })
+      //   });
+
+    return this.database.ex(
+      'all',
+      `
+        SELECT 
+          timestamp,file 
+        FROM 
+          [screenshots] 
+        WHERE 
+          datestamp = @date
+        ORDER BY timestamp`, 
+        {
+          '@date': this.dateString(filterOptions.dateMatching)
+        }
+        ).
+      then(rows => {
+        rows.forEach(row => {
+          console.log(new Date(row.timestamp));
+        });
+
+        return rows.map(row => {
+          return {
+            timestamp: row.timestamp,
+            file: row.file 
+          }
+        })
+      });
+  }
+
+  private dateString(date: Date) {
+    return format(date, 'yyyy-MM-dd'); // YYYY-MM-DD
+  }
+
+  private dateTimeString(date: Date) {
+    return format(date, 'yyyy-MM-ddTHH:mm'); // YYYY-MM-DD
   }
 }
 
--- test/integration/database/marine-weather-examples.ts
+++ test/integration/database/marine-weather-examples.ts
@@ -6,6 +6,7 @@ import { MarineWeather, Screenshot } from '../../../src/adapters/database/marine
 import { readFileSync } from 'fs';
 import { resolve, join } from 'path';
 import { ConsoleLog } from '@test/../src/core/logging/log';
+import stringify from '@test/../src/core/stringify';
 
 describe('[marine-weather] Can store screenshots', () => {
   let marineWeather: MarineWeather = null;
@@ -26,5 +27,31 @@ describe('[marine-weather] Can store screenshots', () => {
     const attachments: Screenshot[] = await marineWeather.listScreenshots();
 
     expect(attachments.length).to.eql(1);
+  });
+
+  it('can query for screenshots by date', async () => {
+    const file = readFileSync(resolve(join(__dirname, './diary/samples/2021-07-09.png')));
+
+    await marineWeather.addScreenshot({ file, timestamp: new Date('01-Nov-1999')});
+    await marineWeather.addScreenshot({ file, timestamp: new Date('02-Nov-1999')});
+    await marineWeather.addScreenshot({ file, timestamp: new Date('03-Nov-1999')});
+
+    expect((await marineWeather.listScreenshots({ dateMatching: new Date('31-Oct-1999') })).length).to.eql(0);
+    expect((await marineWeather.listScreenshots({ dateMatching: new Date('01-Nov-1999') })).length).to.eql(1);
+    expect((await marineWeather.listScreenshots({ dateMatching: new Date('02-Nov-1999') })).length).to.eql(1);
+    expect((await marineWeather.listScreenshots({ dateMatching: new Date('03-Nov-1999') })).length).to.eql(1);
+    expect((await marineWeather.listScreenshots({ dateMatching: new Date('04-Nov-1999') })).length).to.eql(0);
+  });
+
+  it('can query for screenshots by date and time', async () => {
+    const file = readFileSync(resolve(join(__dirname, './diary/samples/2021-07-09.png')));
+
+    await marineWeather.addScreenshot({ file, timestamp: new Date('02-Nov-1999')});
+
+    //console.log(stringify(await marineWeather.listScreenshots({ dateMatching: new Date('02-Nov-1999') })));
+
+    expect((await marineWeather.listScreenshots({ dateMatching: new Date('02-Nov-1999 00:00:00') })).length).to.eql(1);
+    expect((await marineWeather.listScreenshots({ dateMatching: new Date('02-Nov-1999 00:00:01') })).length).to.eql(1);
+    expect((await marineWeather.listScreenshots({ dateMatching: new Date('02-Nov-1999 23:59:59') })).length).to.eql(1);
   });
 });
